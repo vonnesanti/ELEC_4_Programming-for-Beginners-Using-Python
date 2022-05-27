d0a3fd5a8ccf315cf0f0caaecd8cf256ce5792ce
Yvonne Santiago <ybsantiago@iskolarngbayan.pup.edu.ph>
2022-05-28 02:26:31 +0800

    This commit is for EXercise 2.7

---
+++ 2.7_Coding Exercise.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+favorite_color = input("Enter your favorite color: ")
+favorite_movie = input("Enter your favorite movie: ")
+mobile_number = int(input("Enter your 11-digit mobile number: "))
+motto_in_life = input ("Enter your motto in life: ")
